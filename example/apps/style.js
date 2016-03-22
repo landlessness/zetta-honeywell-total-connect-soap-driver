31d27b3ac5c7661c8a071bc6ffe04fa19f4e6e29
landlessness <brian@landlessness.net>
2016-03-22 18:50:43 -0400

    dropped image pref feature for now

--- example/apps/style.js
+++ example/apps/style.js
@@ -5,21 +5,12 @@ var IMAGE_URL_ROOT = 'http://www.zettaapi.org/icons/';
 var IMAGE_EXTENSION = '.png';
 
 var stateImageForDevice = function(device) {
-  var typeState = device.type + '-' + device.state;
-  
-  var preferredImageFor = {
-    'security-arming': 'security-arming-stay'
-  }
-
-  var preferredImage = preferredImageFor[typeState];
-  if (typeof preferredImage !== 'undefined') {
-    typeState = preferredImage;
-  }
-
-  return IMAGE_URL_ROOT + typeState + IMAGE_EXTENSION;
+  return IMAGE_URL_ROOT + device.type + '-' + device.state + IMAGE_EXTENSION;
 }
 
 module.exports = function(server) {
+  console.log('util.inspect(server): ' + util.inspect(server));
+
   ['security', 'light', 'camera'].forEach(function(deviceType){
     var deviceQuery = server.where({ type: deviceType});
     server.observe([deviceQuery], function(device) {
@@ -33,14 +24,25 @@ module.exports = function(server) {
           cb();
         },
         fields: [
-          {name: 'imageURL', type: 'text'}
+          {name: 'image', type: 'text'}
         ]
       };
 
+      device.call('update-state-image', stateImageForDevice(device));
       var stateStream = device.createReadStream('state');
       stateStream.on('data', function(newState) {
         device.call('update-state-image', stateImageForDevice(device));
       });
+
+      var hideUpdateStateImageAction = {
+        action: 'update-state-image',
+        display: 'none'
+      };
+      if (typeof device.style.actions === 'undefined' || device.style.actions.constructor !== Array) {
+        device.style.actions = [hideUpdateStateImageAction];
+      } else {
+        device.style.actions.push(hideUpdateStateImageAction);
+      }
       
     });
   });
